34f899bb1188c1c03f60c042df1466d54e6b4ab6
JensDeCraecker <jens.decraecker@student.ap.be>
2017-12-07 17:42:35 +0100

    google api fixed

--- public/js/js.js
+++ public/js/js.js
@@ -1,4 +1,4 @@
-function succes(position) {
+/*function succes(position) {
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
     console.log(lat, long);
@@ -18,4 +18,26 @@ function initMap() {
   });
 }
 
-navigator.geolocation.getCurrentPosition(succes);
+navigator.geolocation.getCurrentPosition(succes);*/
+
+function initMap() {
+  const uluru = {lat: 51.230016, lng: 4.416220};
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: uluru
+  })
+
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  })
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      }
+      map.setCenter(pos)
+    })
+  }
+}
